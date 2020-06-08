92bcea6164332a7b74252b68be502feefa5df906
vichmi <vichmibg@mail.bg>
2020-06-08 22:09:10 +0300

    Basic mp

---
+++ game.js
@@ -0,0 +1,131 @@
+var socket = io();
+endlessCanvas = true;
+function lerp(a, b, t) {
+	return {x: a.x + (b.x - a.x) * t, y: a.y + (b.y - a.y) * t};
+}
+
+function p3curve(a, b, c, t) {
+	let l0 = lerp(a, b, t);
+	let l1 = lerp(b, c, t);
+	return lerp(l0, l1, t);
+}
+
+function p4curve(a, b, c, d, t) {
+	let l0 = p3curve(a, b, c, t);
+	let l1 = p3curve(b, c, d, t);
+	return lerp(l0, l1, t);
+}
+
+const bs = 10, blocks = 200;
+let p = [], id = -1, height = [];
+let b = [];
+
+socket.on('init', (p_, terr, id_) => {
+    p = p_;
+    height = terr;
+    id = id_;
+});
+socket.on('players', (p_, id_) => {
+    p[id_] = p_;
+});
+socket.on('bullets', (b_) => {
+    b = b_;
+});
+let isDead = false;
+function update() {
+    if(id == -1) return;
+    let isMoving = false, s = 3, isFly = true;
+    if(isFly) {
+        isMoving = true;
+        p[id].y+=s;
+    }
+    if(isKeyPressed[65]) {p[id].x-=s;isMoving = true}
+    if(isKeyPressed[68]) {p[id].x+=s;isMoving = true}
+    for(let i=0;i<blocks;i++) {
+        if(areColliding(p[id].x, p[id].y, p[id].w, p[id].h, i*bs, height[i], bs, bs)) {
+            p[id].y = height[i]-p[id].h;
+            isFly = false;
+        }
+    }
+    if(isMoving) {
+        socket.emit('move', p[id]);
+    }
+    if(p[id].hp<=0) {
+        p[id].connected = false;
+        socket.emit('dead', id);
+    }
+}
+
+function drawHealthbar(x, y, helth, maxHelth) {
+    context.fillStyle = "red";
+    context.fillRect(x,y,maxHelth, 10);
+    context.fillStyle = "green";
+    context.fillRect(x,y,helth,10);
+}
+
+function minimap() {
+    context.save();
+    context.scale(0.1, 0.1);
+    context.strokeRect(0,0,2000, canvas.height);
+    for(let i=0;i<p.length;i++) {
+        if(p[i].connected) {
+            if(i==id) context.fillStyle = "red"; else context.fillStyle = "blue";
+            context.fillRect(p[i].x, p[i].y, p[i].w, p[i].h);
+            // context.fillRect(p[i].x+(p[i].w/2)-p[i].hp/2, p[i].y-15, p[i].hp, 5);
+            drawHealthbar(p[i].x+(p[i].w/2)-50, p[i].y-15, p[i].hp, 100)
+        }
+    }
+    context.fillStyle = 'black';
+    for(let i=0;i<blocks-2;i+=2) {
+        for(let j=0;j<1;j+=.05) {
+            let a = {x:i*bs, y:height[i]}
+            let b = {x:(i+1)*bs, y:height[i+1]}
+            let c = {x:(i+2)*bs, y:height[i+2]}
+            // let d = {x:(i+3)*bs, y:height[i+3]}
+            let e = p3curve(a, b, c, j);
+            context.fillRect(e.x, e.y, bs, bs);
+        }
+    }
+    context.restore();
+}
+
+function draw() {
+    context.save();
+    // context.strokeRect(0,0,800,600);
+    for(let i=0;i<p.length;i++) {
+        if(p[i].connected) {
+            if(i==id) context.fillStyle = "red"; else context.fillStyle = "blue";
+            context.fillRect(p[i].x, p[i].y, p[i].w, p[i].h);
+            // context.fillRect(p[i].x+(p[i].w/2)-p[i].hp/2, p[i].y-15, p[i].hp, 5);
+            drawHealthbar(p[i].x+(p[i].w/2)-50, p[i].y-15, p[i].hp, 100)
+        }
+    }
+    context.fillStyle = 'black';
+    for(let i=0;i<b.length;i++) {
+        context.beginPath();
+        context.arc(b[i].x, b[i].y, 3, 0, 2*Math.PI);
+        context.fill();
+    }
+    for(let i=0;i<blocks-2;i+=2) {
+        for(let j=0;j<1;j+=.05) {
+            let a = {x:i*bs, y:height[i]}
+            let b = {x:(i+1)*bs, y:height[i+1]}
+            let c = {x:(i+2)*bs, y:height[i+2]}
+            // let d = {x:(i+3)*bs, y:height[i+3]}
+            let e = p3curve(a, b, c, j);
+            context.fillRect(e.x, e.y, bs, bs);
+        }
+    }
+    minimap();
+    context.restore();
+}
+
+function keyup(key) {
+	console.log("KEY:",key);
+}
+function mouseup() {
+    console.log("X:",mouseX,"Y:", mouseY);
+    if(p[id].connected) {
+        socket.emit('shoot', mouseX, mouseY, id);
+    }
+}
---
+++ server.js
@@ -0,0 +1,196 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = 3000;
+
+function getFiles(name, file) {
+    app.get('/'+name, (req, res) => {
+        res.sendFile(__dirname+'/'+file);
+    })
+}
+getFiles('/', 'start.html')
+getFiles('game.js', 'game.js');
+
+class Player {
+    constructor(id, x, y) {
+        this.x = Math.floor(Math.random()*800);
+        this.y = y;
+        this.w = 30;
+        this.h = 30;
+        this.hp = 100;
+        this.id = id;
+        this.connected = true;
+    }
+}
+class Bullet {
+    constructor(x, y, tx, ty, id) {
+        this.x = x;
+        this.y = y;
+        this.richochets = 0;
+        let d = Math.sqrt( (tx-x)*(tx-x)+(ty-y)*(ty-y));
+        this.dx = (tx-x)/d;
+        this.dy = (ty-y)/d;
+        this.dx*=10;
+        this.dy*=10;
+        this.id = id;
+        this.dmg = 10;
+    }
+    move() {
+        this.x+=this.dx;
+        this.y+=this.dy; 
+    }
+}
+class Weapon {
+    constructor(x ,y, type) {
+        this.x = x;
+        this.y = y;
+        this.s = p.s;
+        this.type = type;
+        if(this.type == 'pistol') {
+            this.w = 40;
+            this.h = 21;
+            this.dmg = 100;
+        }
+        if(this.type == 'rpg') {
+            this.w = 69;
+            this.h = 36;
+            this.dmg = 200;
+        }
+        this.bullets = 1000;
+        // this.bulletsHolding = bulletsHolding; // ms for the moment to shoot
+        this.b = [];
+    }
+    shoot(tx, ty) {
+        if(this.bullets>0) {
+            this.bullets--;
+            this.b.push(new Bullet(this.x+this.angle, this.y, tx, ty));
+        }
+    }
+    move() {
+        this.tx = mouseX;
+        this.ty = mouseY;
+        for(let i=0;i<this.b.length;i++) {
+            if(this.type == 'pistol') {
+                this.b[i].move(1);
+            }
+            if(this.type == 'rpg') {
+                this.b[i].move(0.5);
+            }
+        }
+        if(isKeyPressed[65] || isKeyPressed[37]) {this.x-=this.s}
+        if(isKeyPressed[68] || isKeyPressed[39]) {this.x+=this.s}
+    }
+    draw() {
+        this.angle = Math.atan2(this.ty-this.y, this.tx-this.x);
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.fillStyle = "black";
+        if(mouseX>this.x) {
+            context.fillRect(0, 0, this.w, this.h);
+        }else{
+            context.fillRect(0, -30, this.w, this.h);
+        }
+        context.restore();
+    }
+}
+
+let blocks = 400;
+function createTerrain() {
+    let height = [];
+    function smooth(inp) {
+        let newHeight = [];
+        for(let i=0;i<inp.length-7;i++) {
+            newHeight[i] = (inp[i]+inp[i+1]+inp[i+2]+inp[i+3]+inp[i+4]+inp[i+5]+inp[i+6]+inp[i+7])/8;
+        }
+        return newHeight;
+    }
+    const px = 40;
+    height[0] = 550;
+    for(let i=1;i<blocks;i++) {
+        height[i] = height[i-1]+Math.floor(Math.random()*px-(px/2));
+    }
+    return height = smooth(height);
+}
+
+let p = [], terrain = createTerrain(), b = [];
+let clients = 0;
+io.on('connection', (socket) => {
+    let id = clients;
+    for (let i=0;i<clients;i++){
+        if (p[i].x != p[i].x){
+            id = i;
+            break;
+        }
+    }
+    console.log("Client:",id," has joined.")
+    p.push(new Player(id, blocks*5, terrain[Math.floor(Math.random()*blocks)]-30));
+    clients++;
+    socket.emit('init', p, terrain, id);
+    socket.broadcast.emit('players', p[id], id);
+
+    socket.on('move', (p_) => {
+        p[id] = p_;
+        socket.broadcast.emit('players', p[id], id);
+    });
+
+    socket.on('shoot', (tx, ty, id_) => {
+        b.push(new Bullet(p[id_].x, p[id_].y, tx, ty, id_));
+    });
+
+    socket.on("dead", function(id_) {
+        p[id_].connected = false;
+        io.emit("players", p[id_],id_,);
+    });
+ 
+    socket.on("disconnect", function(){
+        p[id].connected = false;
+        p[id].x = undefined;
+        p[id].y = undefined;
+        io.emit("players", p[id], id);
+    });
+});
+
+function areColliding(Ax, Ay, Awidth, Aheight, Bx, By, Bwidth, Bheight) {
+	if (Bx <= Ax + Awidth) {
+		if (Ax <= Bx + Bwidth) {
+			if (By <= Ay + Aheight) {
+				if (Ay <= By + Bheight) {
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+function removeBullet(index) {
+    b[index] = b[b.length-1];
+    b.pop();
+}
+function update() {
+    for(let i=0;i<b.length;i++) {
+        b[i].move();
+        for(let j=0;j<p.length;j++) {
+            if(areColliding(b[i].x, b[i].y, 5, 5, p[j].x, p[j].y, p[j].w, p[j].h) && p[j].connected) {
+                if(b[i].id!=p[j].id) {
+                    p[j].hp-=b[i].dmg;
+                    removeBullet(i);
+                    io.emit("players", p[j], j);
+                    break;
+                }
+            }
+        }
+        if(i >= b.length) break;
+        if(b[i].x<0 || b[i].x>=blocks*5 || b[i].y<0 || b[i].y>=height.max) {
+            removeBullet(i);
+        }
+        
+    }
+    io.emit('bullets', b);
+}
+setInterval(update, 10);
+
+
+http.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
